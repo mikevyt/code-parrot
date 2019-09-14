4fbdfd8a8fff46e0f4d41a4a3a610197f7fc16d3
Michael Vytlingam <michael.vytlingam@gmail.com>
2019-09-14 00:42:16 -0400

    Adds GeneratedCode component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Grid } from 'semantic-ui-react';
+import GeneratedCode from './GeneratedCode';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Grid>
+                <Grid.Column>
+                    {/* <UploadArea /> */}
+                </Grid.Column>
+                <Grid.Column>
+                        <GeneratedCode />
+                </Grid.Column>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/GeneratedCode/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+import './GeneratedCode.css';
+
+const GeneratedCode = () => (
+    <Segment className="generated-code">
+        <div>
+            for i in range(10):
+        </div>
+        <div>
+            print(hello)
+        </div>
+    </Segment>
+);
+
+export default GeneratedCode;
