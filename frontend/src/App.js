2170e6525a649c992743efd35962bf4ad2adc37d
Michael Vytlingam <michael.vytlingam@gmail.com>
2019-09-14 12:47:02 -0400

    Adds css framework and adds code editor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import GeneratedCode from './GeneratedCode';
+import AceEditor from 'react-ace';
+
+import "brace/mode/python";
+import "brace/theme/github";
+
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <Grid>
-                <Grid.Column>
-                    {/* <UploadArea /> */}
+            <Grid columns={2} divided style={{height: '100vh'}}>
+                <Grid.Column width={8}>
+                    {/* <AceEditor 
+                        mode="python"
+                        theme="github"
+                        value={'for i in range(10):\n\tdong'}
+                        style={{height: '95vh'}}
+                    /> */}
                 </Grid.Column>
-                <Grid.Column>
-                        <GeneratedCode />
+                <Grid.Column width={8}>
+                    <AceEditor 
+                        mode="python"
+                        theme="github"
+                        value={'for i in range(10):\n\tdong'}
+                        style={{height: '95vh', width: 'auto'}}
+                    />
                 </Grid.Column>
             </Grid>
         </div>
