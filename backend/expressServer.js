50bc988edbc540839883845d457064a6e109d1e2
Michael Vytlingam <michael.vytlingam@gmail.com>
2019-09-15 02:14:28 -0400

    Refactor server files

--- backend/expressServer.js
+++ backend/expressServer.js
@@ -5,6 +5,7 @@ var cors = require('cors');
 const SpeechToText = require('./APIManager');
 const multer = require('multer'); //use multer to upload blob data
 const upload = multer(); // set multer to be the upload variable (just like express, see above ( include it, then use it/set it up))
+const CodeConverter = require("./CodeConverter");
 
 app.use(cors());
 
@@ -20,6 +21,32 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     const text = apiManager.uploadFile(req.file.originalname, text => {
         res.send(text);
     });
+});
+
+const codeConverter = new CodeConverter();
+  result = codeConverter.parseKeywordsFromString();
+
+  if (result.intent == "End") {
+    let sentence = "\n";
+
+  } else if (result.intent == "MakeFunction") {
+    let sentence = createFunction(result.name, result.param1, result.param2);
+
+  } else if (result.intent == "ForLoop") {
+    let sentence = initializeForLoop(result.interations);
+
+  } else if (result.intent == "IfStatement") {
+    let sentence = createIfStatement(result.x, result.condition, result.y);
+
+  } else if (result.intent == "CallFunction") {
+    let sentence = callFunction(result.funcName, result.arg1, result.arg2);
+
+  } else if (result.intent == "InitializeVariable") {
+    let sentence = initializeVariable(result.name, result.value);
+
+  } else {  //intent == PrintText
+    let sentence = printSomething(result.sentence);
+  }
 });
 
 app.listen(8000, () => {
