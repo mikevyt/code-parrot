6d1f91badc81d7d32fb28a1dade559b8b97fc8f9
Anne Adriano <annec.adriano@gmail.com>
2019-09-15 01:33:34 -0400

    if statements added to server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const CodeConverter = require("./CodeConverter");
 
+
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
 
@@ -12,7 +13,31 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
   const codeConverter = new CodeConverter();
-  codeConverter.getKeywords();
+  result = codeConverter.getKeywords();
+
+  if (result.intent() == "End"){
+    let sentence = "\n";
+     
+  }else if (result.intent == "MakeFunction"){
+    let sentence = createFunction(result.name, result.param1, result.param2);
+
+  }else if (result.intent == "ForLoop"){
+    let sentence = initializeForLoop(result.x, result.interations);
+
+  }else if (result.intent == "IfStatement"){
+    let sentence = createIfStatement(result.x, result.condition, result.y);
+  
+  }else if (result.intent == "CallFunction"){
+    let sentence = callFunction(result.funcName, result.arg1, result.arg2);
+
+  }else if (result.intent == "InitializeVariable"){
+    let sentence = initializeVariable(result.name, result.value);
+
+  }else  //intent == PrintText
+    let sentence = printSomething(result.sentence); 
+}
+  
+
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
